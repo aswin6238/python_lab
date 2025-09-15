ad83f2edf4ac0ecdd3b5f8a5769e18669c8696b5
aswin6238 <aswinarun6373@gmail.com>
2025-09-15 15:38:27 +0530

    Add files via upload

---
+++ list_of_integers.py
@@ -0,0 +1,21 @@
+
+
+
+n = int(input("Enter the size: "))
+
+nlist = []
+flist = []
+i = 0
+
+while(i < n):
+    nlist.append(int(input()))
+    i += 1
+
+for i in nlist:
+    
+    if i > 100:
+        i = "over"
+        f
+
+
+print(nlist)
