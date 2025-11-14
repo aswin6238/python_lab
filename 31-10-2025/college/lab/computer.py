f329506944d9575615e232888fd15c5937b4ee74
aswin6238 <aswinarun6373@gmail.com>
2025-11-14 11:46:11 +0530

    Add files via upload

---
+++ 31-10-2025/college/lab/computer.py
@@ -0,0 +1,3 @@
+def lab():
+    print("The total no:of computers is 43")
+
---
+++ 31-10-2025/package_example.py
@@ -0,0 +1,7 @@
+from college import student
+from college import staffs
+from college.lab import computer
+
+student.stud()
+staffs.staff()
+computer.lab()
